0bf34927df30b3323267d6e8cf3aa5a5e87a5bac
AnhMinh0203 <Leanhminh0203@gmail.com>
2025-11-25 07:21:21 +0700

    develope: body of home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Banner } from "../../layouts/banner/banner";
 import { Navbar } from "../../layouts/navbar/navbar";
+import { Body } from "../../layouts/body/body";
 
 @Component({
   selector: 'app-home',
-  imports: [Banner, Navbar],
+  imports: [Banner, Navbar, Body],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
